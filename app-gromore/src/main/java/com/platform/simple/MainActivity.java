844849add7c0c66096a72e97787c21511f5bb26f
codessource <18186950091@163.com>
2022-11-11 12:53:46 +0800

    apk version update

--- app-gromore/src/main/java/com/platform/simple/MainActivity.java
+++ app-gromore/src/main/java/com/platform/simple/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.platform.lib.bean.Result;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv_version)).setText(String.format("SDK版本:v%s", PlatformManager.getInstance().getVersion()));
         requstPermission();
     }
 
--- app-topon/src/main/java/com/platform/simple/MainActivity.java
+++ app-topon/src/main/java/com/platform/simple/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.anythink.core.api.ATSDK;
@@ -41,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv_version)).setText(String.format("SDK版本:v%s", PlatformManager.getInstance().getVersion()));
         requstPermission();
     }
 
